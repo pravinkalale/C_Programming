acc86a6d3d433e57ac68cfe981b1896ea95a200d
Pravin Kalale <kalalepravin2@gmail.com>
2026-09-04 23:57:59 +0530

    Add C Program Files

---
+++ mock.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+
+    printf("Enter Number:");
+    scanf("%d",&a);
+
+    if (a%2==0)
+    {
+        printf("Even number");
+    }
+    else
+    {
+        printf("Odd number");
+    }
+    
+}
---
+++ posineg.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    
+    printf("Enter Number:");
+    scanf("%d",&a);
+
+    if (a>0)
+    {
+        printf("Number is positive");
+    }
+    else if (a<0)
+    {
+        printf("Number is negative");
+    }
+    else
+    {
+        printf("Number is zero");
+    }
+    
+    return 0;
+}
